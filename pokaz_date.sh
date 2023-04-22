9f49e3d1579cdf765e9e3d7094ecde0c775e1a4d
Acafax <131300683+Acafax@users.noreply.github.com>
2023-04-22 14:31:36 +0200

    Update pokaz_date.sh

--- pokaz_date.sh
+++ pokaz_date.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-date +%Y-%m-%d
+
+while [[ $# -gt 0 ]]
+do 
+key="$1"
+
+case $key in 
+    --date)
+    date=$(date +%Y-%m-%d)
+    echo "dzisijsza data to $date"
+    shift
+    ;;
+    *)
+    echo "Nieznana flaga $key"
+    exit 1
+    ;;
+esac
+done
