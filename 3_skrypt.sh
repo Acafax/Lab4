4ca9e070b67d14d6b50fd0884b3b512a4a69ca62
Acafax <rudywiktor.markiewicz1@dsw.edu.pl>
2023-04-22 15:35:06 +0200

    uwaga bo robi syf w plikach

---
+++ 3_skrypt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for i in {1..100}
+do
+    nazwa_pliku="file$i.txt"
+    touch "$nazwa_pliku"
+    echo "ten plik nazywa sie $nazwa_pliku" >> "$nazwa_pliku"
+    echo "jest on plikiem numer $i" >> "$nazwa_pliku"
+    echo "dzisiajsza data to $(date +%Y-%m-%d)" >>"$nazwa_pliku"
+done
