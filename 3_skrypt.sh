de8ec8c40dbe08be962333748e0ee82c3e4b427e
Acafax <131300683+Acafax@users.noreply.github.com>
2023-04-22 22:01:07 +0200

    Update 3_skrypt.sh

--- 3_skrypt.sh
+++ 3_skrypt.sh
@@ -4,6 +4,7 @@ for i in {1..100}
 do
     nazwa_pliku="log$i.txt"
     touch "$nazwa_pliku"
+    echo "ten plik został stowrzony przez skrypt o nazwie '3_skrypt'" >> "$nazwa_pliku"  
     echo "ten plik nazywa sie $nazwa_pliku" >> "$nazwa_pliku"
     echo "jest on plikiem numer $i" >> "$nazwa_pliku"
     echo "dzisiajsza data to $(date +%Y-%m-%d)" >>"$nazwa_pliku"
